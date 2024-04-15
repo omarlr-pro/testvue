07aa2338507fcc0040f2e4515c74e76ccd44672f
omarlr-pro <laromar08@gmail.com>
2024-04-15 11:42:24 +0100

    test

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app"> 
+   <HeaderComponent message="waaa"/>
+   <div class="main-content">
+
+      <HelloWorld msg="salim " />
+      <MainComponent message="azdiiiin salim">
+        <router-view> </router-view>
+      </MainComponent>
+    </div>
+  <FooterComponent message="je suis le footer"/>
+      
+   </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FooterComponent from './components/layout/FooterComponent.vue'
+import MainComponent from './components/layout/MainComponent.vue'
+import HeaderComponent from './components/layout/HeaderComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    HelloWorld,
+    FooterComponent,
+    MainComponent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/layout/FooterComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{message}}</h1>
+   <h2>hihi</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String ,
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  color: red;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
